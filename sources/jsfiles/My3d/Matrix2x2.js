17dd4462a78393c9cc8869e170d22af60590667c
rickey930 creatures <rickey930creatures@gmail.com>
2014-05-14 16:32:13 +0900

    シンプル化

---
+++ sources/jsfiles/My3d/Matrix2x2.js
@@ -0,0 +1,9 @@
+//=======================
+//  Matrix2x2クラス
+//=======================
+function Matrix2x2(mt11, mt12, mt21, mt22) {
+	this.m11 = mt11;
+	this.m12 = mt12;
+	this.m21 = mt21;
+	this.m22 = mt22;
+}
