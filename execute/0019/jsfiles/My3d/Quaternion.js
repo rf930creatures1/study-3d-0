fb361171c2983426f4f32dd49bd043c8d2c154eb
rickey930 creatures <rickey930creatures@gmail.com>
2014-07-01 19:28:56 +0900

    任意軸回転行列、回転軸原点変更機能付き任意軸回転行列メソッドの追加 (Matrix4x4.js)

---
+++ execute/0019/jsfiles/My3d/Quaternion.js
@@ -0,0 +1,59 @@
+//=====================
+// Quaternionクラス
+//=====================
+
+function Quaternion(x, y, z, w) {
+	if (x == null) x = 0;
+	if (y == null) y = 0;
+	if (z == null) z = 0;
+	if (w == null) w = 0;
+	this.x = x;
+	this.y = y;
+	this.z = z;
+	this.w = w;
+}
+
+//クォータニオン同士の掛け算
+Quaternion.prototype.multiply = function(q) {
+	var vec3I = new Vector3(this.x, this.y, this.z);
+	var vec3II = new Vector3(q.x, q.y, q.z);
+	var w = this.w * q.w - vec3I.dot(vec3II);
+	var cross = vec3I.cross(vec3II);
+	return new Quarternion(cross.x + this.w * q.x + this.x * q.w, 
+						   cross.y + this.w * q.y + this.y * q.w, 
+						   cross.z + this.w * q.z + this.z * q.w, 
+						   w);
+}
+
+//共役クォータニオン
+Quaternion.prototype.conjugate = function() {
+	return new Quaternion(-this.x, -this.y, -this.z, this.w);
+}
+
+//回転クォータニオン
+Quaternion.prototype.transfrom = function(vec3, rad) {
+	var p = new Quaternion(vec3.x, vec3.y, vec3.z, 0);
+	var radian = rad / 2:
+	var sin = Math.sin(radian);
+	var cos = Math.cos(radian);
+	var q = new Quaternion(vec3.x * sin, vec3.y * sin, vec3/z * sin, cos);
+	var r = q.conjugate();
+	var rp = r.multiply(p);
+	return rp.multiply(q);
+}
+
+//回転行列生成
+Quaternion.prototype.rotationMatrix = function() {
+	return new Matrix4x4(
+		1 - 2 * (Math.pow(this.y, 2) + Math.pow(this.z, 2)), 2 * (this.x * this.y - this.w * this.z), 2 * (this.w * this.y + this.x * this.z), 0,
+		2 * (this.x * this.y + this.w * this.z), 1 - 2 * (Math.pow(this.x, 2) + Math.pow(this.z, 2)), 2 * (this.y * this.z + this.w * this.x), 0,
+		2 * (this.x * this.z + this.w * this.y), 2 * (this.y * this.z + this.w * this.x), 1 - 2 * (Math.pow(this.x, 2) + Math.pow(this.y, 2)), 0,
+		0, 0, 0, 1);
+}
+
+/*
+Quaternion.makeFromMatrix = function(_matrix4x4) {
+	var m = _matrix4x4;
+	
+}
+*/
---
+++ execute/0019/jsfiles/Scene/Scene0.js
@@ -0,0 +1,165 @@
+//=======================
+//  Scene0クラス
+//=======================
+function Scene0(gameManager) {
+	GameScene.call(this, gameManager);
+	
+}
+Scene0.prototype = new GameScene(this.gManager);
+Scene0.prototype.init = function() {
+	this.guide3d = new Guide3d(20);
+	
+	//位置データ (ワールド座標)
+	this.position = new Vector3(0, 0, 0);
+	
+	//大きさ
+	this.radius = 100;
+	
+	//モデルデータ (ローカル座標)
+
+	//頂点
+	var mp = [new Vector3(0, 10, 0), new Vector3(10, -10, 0), new Vector3(-10, -10, 0)];
+	//モデル (nullを入れて面データ(MoveToとlineToの使い分けフラグ)にしている)
+	this.model = new Model([new Polygon(
+					//時計回りに描くと表になる。
+					mp[0], mp[1], mp[2] 
+					
+				)]);
+
+/*
+	var mPolygons = [];
+	for (var i in obj3dFile.groups[0].surfaces) {
+		var polygons = Polygon.surfaceToTriangles(obj3dFile.groups[0].surfaces[i]);
+		for (var j in polygons) {
+			mPolygons.push(polygons[j])
+		}
+	}
+	this.model = new Model(mPolygons);
+*/
+
+	this.xrot = 0;
+	this.yrot = 0;
+	this.zrot = 0;
+	this.autorot = 0;
+	
+	//マウスの直前までの状態
+	this.mouseButton = false;
+	//マウスが最初に押されたときの点
+	this.mouseOX = 0;
+	this.mouseOY = 0;
+	//マウスが押される直前のxyrot
+	this.gxrot = 0;
+	this.gyrot = 0;
+	
+	//投影変換どちらを使うか spaceキーで切り替え
+	this.seisyaei = false;
+	//space押しっぱなしでぱっぱと変更しないように、投影変換できるかフラグ
+	this.seisyaeiChangePermissition = true;
+}
+
+Scene0.prototype.disp = function(canvas) {
+	canvas.save();
+	canvas.clearRect(0, 0, canvas.canvas.width, canvas.canvas.height);
+	canvas.strokeStyle = (new Color(255, 255, 0, 0)).toContextString();
+	canvas.fillStyle = (new Color(128, 0, 192, 0)).toContextString();
+	
+	
+	var wMat = Matrix4x4_Identity();
+	wMat.translate(this.position.x, this.position.y, this.position.z);
+	
+	//任意軸回転
+	var rotGenten = new Vector3(10, 0, 0);
+	var saarMat = Matrix4x4_SuperArbitraryAxisRotate(rotGenten , new Vector3(0, 1, 0), CircleCalculator.toRadian(this.autorot));
+	wMat.multiply(saarMat); //以上の情報をワールドマトリックスに格納する
+	//任意軸回転ここまで
+	
+	this.model.World(wMat);
+	
+	var at = new Vector3(0, 0, 0);			//注視点(カメラの向き)
+	var eye = new Vector3(0, 0, 0);			//視点	(カメラの場所)
+	var up = new Vector3(0, 1, 0);			//上	(カメラ自身の回転)
+	var eyeMat = Matrix4x4_Identity();
+	var upMat = Matrix4x4_Identity();
+	eyeMat.rotateY(CircleCalculator.toRadian(this.yrot));
+	eyeMat.rotateX(CircleCalculator.toRadian(this.xrot));
+	eyeMat.rotateZ(CircleCalculator.toRadian(this.zrot));
+	//upMat.rotateZ(CircleCalculator.toRadian(this.zrot));
+	eyeMat.translate(0, 0, -50);
+	eye = eyeMat.transform(eye);
+	up = upMat.transform(up);
+	
+	var near = 30;
+	var far = 50;
+	var nearPlaneWidth = 40;
+	
+	this.model.Camera(at, eye, up);
+	if (this.seisyaei)
+		this.model.OrthogonalProjection(-nearPlaneWidth/2, nearPlaneWidth/2, nearPlaneWidth/2, -nearPlaneWidth/2, near, far);
+	else
+		this.model.Perspective(-nearPlaneWidth/2, nearPlaneWidth/2, nearPlaneWidth/2, -nearPlaneWidth/2, near, far);
+	this.model.Screen(canvas);
+	
+	var fillColors = [
+		new Color(255, 192, 0, 0), new Color(255, 192, 0, 0), 
+		new Color(255, 0, 192, 0), new Color(255, 0, 192, 0), 
+		new Color(255, 0, 0, 192), new Color(255, 0, 0, 192), 
+		new Color(255, 0, 192, 192), new Color(255, 0, 192, 192), 
+		new Color(255, 192, 192, 0), new Color(255, 192, 192, 0), 
+		new Color(255, 192, 0, 192), new Color(255, 192, 0, 192)
+	];
+	
+	this.model.draw(canvas, null, fillColors);
+	
+	//3dガイドライン
+	if (this.seisyaei) this.guide3d.projectionType = ProjectionType.SEISHAEI;
+	else this.guide3d.projectionType = ProjectionType.TOUSHI;
+	this.guide3d.draw(canvas, at, eye, up, -nearPlaneWidth/2, nearPlaneWidth/2, nearPlaneWidth/2, -nearPlaneWidth/2, near, far);
+	
+	canvas.restore();
+}
+
+Scene0.prototype.step = function() {
+	this.autorot += 50 * FPS;
+	if (this.gManager.keyInput.mouseButton) {
+		//それまで押されていなかった場合、
+		if (!this.mouseButton) {
+			//移動の原点情報を格納する
+			this.mouseOX = this.gManager.keyInput.mouseX;
+			this.mouseOY = this.gManager.keyInput.mouseY;
+			this.gxrot = this.xrot;
+			this.gyrot = this.yrot;
+		}
+		//角度を決める
+		this.yrot = (this.gManager.keyInput.mouseX - this.mouseOX) + this.gyrot;
+		this.xrot = (this.gManager.keyInput.mouseY - this.mouseOY) + this.gxrot;
+	}
+	else {
+		if (this.gManager.keyInput.left) {
+			this.yrot -= 40 * FPS;
+		}
+		else if (this.gManager.keyInput.right) {
+			this.yrot += 40 * FPS;
+		}
+		if (this.gManager.keyInput.up) {
+			this.xrot -= 40 * FPS;
+		}
+		else if (this.gManager.keyInput.down) {
+			this.xrot += 40 * FPS;
+		}
+		if (this.gManager.keyInput.space) {
+			if (this.seisyaeiChangePermissition) {
+				this.seisyaei = !this.seisyaei;
+			}
+			//一回離すのを待つ
+			this.seisyaeiChangePermissition = false;
+		}
+		else {
+			this.seisyaeiChangePermissition = true;
+		}
+	}
+	this.mouseButton = this.gManager.keyInput.mouseButton;
+}
+
+Scene0.prototype.destroy = function() {
+
+}
